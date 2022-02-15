10fcb96d44c51c231f4ed2bac27b7dc87e7df1d0
Shah-Jahan-Mukhtar <shahjahanmukhtar62@gmail.com>
2022-02-15 15:42:26 -0800

    CarApp Continue

--- App.js
+++ App.js
@@ -1,29 +1,97 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TextInput,
+} from "react-native";
+
 // import Icon from "react-native-icons";
 
 export default function App() {
   return (
     <SafeAreaView style={{ marginTop: 25 }}>
       <View style={styles.container}>
-        <Image
-          source={require("./assets/menu.png")}
-          style={{ marginTop: 20, marginLeft: 20, height: 40, width: 40 }}
-        />
-        <Image
-          source={require("./assets/searchIcon.png")}
+        <View
           style={{
             marginTop: "0%",
-            marginLeft: "70%",
-            height: "15%",
-            width: "8%",
+            height: "50%",
+            width: "100%",
+            flexDirection: "row",
           }}
-        />
-        <Image
-          source={require("./assets/carPic.jpg")}
-          style={{ height: "80%", width: "80%", marginTop: "0%" }}
-        />
+        >
+          <Image
+            source={require("./assets/menu.png")}
+            style={{
+              marginTop: "8%",
+              marginLeft: 20,
+              height: "40%",
+              width: "12%",
+              // flexDirection: "row",
+              // display: "flex",
+            }}
+          />
+          <Image
+            source={require("./assets/searchIcon.png")}
+            style={{
+              marginTop: "10%",
+              marginLeft: "70%",
+              height: "28%",
+              width: "8%",
+              // flexDirection: "row",
+              // display: "flex",
+            }}
+          />
+        </View>
+
+        <View style={{ marginTop: "0%" }}>
+          <Image
+            source={require("./assets/carPic.jpg")}
+            style={{
+              height: "100%",
+              width: "92%",
+
+              marginTop: "0%",
+              marginLeft: "4.4%",
+              borderRadius: 10,
+            }}
+          />
+        </View>
+
         <StatusBar style="auto" />
+        <View
+          style={{
+            // marginTop: "%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={require("./assets/searchIcon.png")}
+            style={{
+              marginLeft: "70%",
+              height: "20%",
+              width: "10%",
+            }}
+          />
+          <TextInput
+            style={{
+              width: "90%",
+              height: "30%",
+
+              borderRadius: 20,
+              shadowColor: "#000",
+              shadowOffset: { width: 0, height: 1 },
+              shadowOpacity: 0.2,
+              shadowRadius: 4,
+              elevation: 1,
+              justifyContent: "center",
+            }}
+            placeholder="Search"
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -34,8 +102,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#FF0000",
     // alignItems: "center",
     // justifyContent: "center",
-    height: 200,
-    flexDirection: "column",
-    display: "flex",
+    height: "51%",
+    // flexDirection: "row",
   },
 });
