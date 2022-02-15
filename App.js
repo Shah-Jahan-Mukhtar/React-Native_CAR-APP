5312ed8a4c7db26505703eca3f9c0a8816246051
Shah-Jahan-Mukhtar <shahjahanmukhtar62@gmail.com>
2022-02-15 14:19:52 -0800

    CarApp screen1

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+// import Icon from "react-native-icons";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ marginTop: 25 }}>
+      <View style={styles.container}>
+        <Image
+          source={require("./assets/menu.png")}
+          style={{ marginTop: 20, marginLeft: 20, height: 40, width: 40 }}
+        />
+        <Image
+          source={require("./assets/searchIcon.png")}
+          style={{
+            marginTop: "0%",
+            marginLeft: "70%",
+            height: "15%",
+            width: "8%",
+          }}
+        />
+        <Image
+          source={require("./assets/carPic.jpg")}
+          style={{ height: "80%", width: "80%", marginTop: "0%" }}
+        />
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FF0000",
+    // alignItems: "center",
+    // justifyContent: "center",
+    height: 200,
+    flexDirection: "column",
+    display: "flex",
   },
 });
