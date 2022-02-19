c9f81ce5db1e582665c11ad7935ca448667b01d4
Shah-Jahan-Mukhtar <shahjahanmukhtar62@gmail.com>
2022-02-19 15:48:55 -0800

    Services Components

--- components/Services.js
+++ components/Services.js
@@ -1,20 +1,24 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import React from "react";
 
 const Services = (props) => {
   return (
     <View>
       <Text
-        style={{
-          padding: 10,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
+        style={[
+          {
+            padding: 10,
+            alignItems: "center",
+            justifyContent: "center",
+          },
+        ]}
       >
         {props.name}
       </Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({});
 
 export default Services;
