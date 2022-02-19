fa2ab2bdeca011bbb5faf608487f7ec095bb907b
Shah-Jahan-Mukhtar <shahjahanmukhtar62@gmail.com>
2022-02-19 14:24:39 -0800

    Services Component

--- components/Services.js
+++ components/Services.js
@@ -1,11 +1,15 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 
 const Services = (props) => {
   return (
     <View>
       <Text
-        style={{ padding: 10, alignItems: "center", justifyContent: "center" }}
+        style={{
+          padding: 10,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
       >
         {props.name}
       </Text>
