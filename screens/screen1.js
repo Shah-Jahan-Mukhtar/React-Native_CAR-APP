59ff56e027c4c0f56823ad7a65607b19fc090610
Shah-Jahan-Mukhtar <shahjahanmukhtar62@gmail.com>
2022-02-24 13:17:35 -0800

    Screen2  design CONT...

---
+++ screens/screen1.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+
+const Screen1 = () => {
+  return (
+    <View>
+      <Text>screen1</Text>
+    </View>
+  );
+};
+
+export default Screen1;
