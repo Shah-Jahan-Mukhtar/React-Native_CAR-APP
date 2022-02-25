7157aa96cdc88b30c6dc57c00d7e4591fd596376
Shah-Jahan-Mukhtar <shahjahanmukhtar62@gmail.com>
2022-02-25 14:17:00 -0800

    screen 2 design

--- screens/screen1.js
+++ screens/screen1.js
@@ -4,8 +4,25 @@ import { Ionicons } from "@expo/vector-icons";
 
 const Screen1 = () => {
   return (
-    <View>
-      <Text>Hello world</Text>
+    <View
+      style={{
+        flex: 1,
+
+        flexDirection: "row",
+      }}
+    >
+      <View style={{ backgroundColor: "#FF8C00", width: "45%" }}>
+        <Text>Hello world</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "#3D3C3A",
+          backfaceVisibility: "visible",
+          width: "55%",
+        }}
+      >
+        <Text>Hello</Text>
+      </View>
     </View>
   );
 };
