fb87636bdf98ff0dc39e1e61f9547143289bcd6b
Shah-Jahan-Mukhtar <shahjahanmukhtar62@gmail.com>
2022-02-26 15:59:42 -0800

    screen

--- screens/screen1.js
+++ screens/screen1.js
@@ -1,6 +1,7 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
+import DeprecatedViewStylePropTypes from "react-native/Libraries/DeprecatedPropTypes/DeprecatedViewStylePropTypes";
 
 const Screen1 = () => {
   return (
@@ -11,7 +12,14 @@ const Screen1 = () => {
         flexDirection: "row",
       }}
     >
-      <View style={{ backgroundColor: "#FF8C00", width: "45%" }}>
+      <View
+        style={{
+          backgroundColor: "#FF8C00",
+          width: "45%",
+
+          alignItems: "center",
+        }}
+      >
         <Image
           source={require("../assets/man.png")}
           style={{ width: "80%", height: "20%", marginTop: "-20%" }}
@@ -19,16 +27,56 @@ const Screen1 = () => {
         <View
           style={{
             borderBottomColor: "black",
-            borderBottomWidth: 1,
-            width: "70%",
+            borderBottomWidth: 1.5,
+            width: "85%",
             justifyContent: "center",
             alignItems: "center",
-            marginLeft: "8%",
+            // marginLeft: "6%",
           }}
         ></View>
-        <View>
-          <Text style={{ fontSize: 15 }}>HANNAH PARIZARES</Text>
+        <View style={{ justifyContent: "center", alignItems: "center" }}>
+          <Text
+            style={{
+              fontSize: 25,
+              fontWeight: "bold",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            HANNAH PARIZARES
+          </Text>
+          <Text>Environmental Science Researcher</Text>
+
+          <Text style={{ fontWeight: "bold", marginTop: 10 }}>SUMMARY </Text>
+          <Text
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              alignContent: "center",
+              textAlign: "center",
+            }}
+          >
+            Supporting our ecosystem and reversing the effects of climate charge
+            are the reasons i get up in the morning.International cooperation is
+            neccesary,but i belive it begins with uncovering new findings and
+            developing exciting technologies through dedication and innovation.
+          </Text>
         </View>
+        <View
+          style={{
+            borderBottomColor: "black",
+            borderBottomWidth: 1.5,
+            width: "85%",
+            marginTop: "10%",
+          }}
+        ></View>
+        <Text style={{ fontWeight: "bold", marginTop: 10 }}>QUALFICATIONS</Text>
+        <Text style={{ textAlign: "center" }}>
+          F.S.c from Govt Zamindar College Gujrat
+        </Text>
+        <Text style={{ textAlign: "center" }}>
+          BS-IT from University Of Gujrat
+        </Text>
       </View>
       <View
         style={{
